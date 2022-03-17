b7077203af4b6deeac7d958695da791a83bf1af2
shakamanaka <camilo.escobar2@utp.edu.co>
2022-03-17 11:16:25 -0500

    First Commit

---
+++ routes/api.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ExampleController;
+use App\Models\User;
+use Faker\Factory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::controller(ExampleController::class)->prefix('example')->group(function (){
+    Route::get('/hello-world','index');
+});
